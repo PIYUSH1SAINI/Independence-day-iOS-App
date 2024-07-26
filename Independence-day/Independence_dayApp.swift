d2ad3366f068c42ba3ae49ed494a96f5f3bcc7dc
PIYUSH1SAINI <piyushsaini20001@gmail.com>
2024-07-26 12:17:36 +0530

    Initial Commit

---
+++ Independence-day/Independence_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Independence_dayApp.swift
+//  Independence-day
+//
+//  Created by Piyush saini on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Independence_dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
